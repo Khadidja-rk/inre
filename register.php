31225e7aed241b5037882938793c81133baef745
Khadidja-rk <khadijapr9@gmail.com>
2025-12-02 20:47:38 +0100

    first commit

---
+++ register.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Register Page</h1>
+    <form action="register.php" method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+
+
+        <input type="submit" value="Register">
+    </form>
+</body>
+
+</html>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = htmlspecialchars($_POST['username']);
+    $email    = htmlspecialchars($_POST['email']);
+
+    $conn = new mysqli("localhost", "siteuser", "kh", "site_db");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $stmt = $conn->prepare("INSERT INTO users (username, email) VALUES (?, ?)");
+    $stmt->bind_param("ss", $username, $email);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        echo "Registration successful for user: " . $username;
+    } else {
+        echo "Error while registering.";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
